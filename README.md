5687b70ce55d0c814e8732d1baa83e0f3b95c8a7
ChrisK-0 <chris.kottisse@hotmail.com>
2022-08-14 15:04:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Small colored logger.
+
+### Usage
+
+Import in and use as:
+
+```
+logger('Hello world')
+or with color:
+logger('ok', 'Hello world')
+```
+
+### Available colors:
+
+`Node terminal colors`
+
+-   `info` Cyan
+-   `ok` Green
+-   `warn` Red
+-   `debug` Yellow/Orange
+-   `silly` Magenta
+-   `white` Default. Used by logger after coloring - `logger('info', 'This will be white')`
---
+++ logger.ts
@@ -0,0 +1,56 @@
+export function logger(color = 'white', text: any = undefined) {
+    const today = new Date()
+
+    
+    function returnTime() {
+        return `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`
+    }
+    
+    const reset = "\x1b[0m" // Used to reset coloring for text.
+    const colors = {
+        "info": "\x1b[36m",
+        "warn": "\x1b[31m",
+        "ok": "\x1b[32m",
+        "silly": "\x1b[35m",
+        "debug": "\x1b[33m",
+        "white": "\x1b[0m"
+    }
+
+    let useTracer = false
+    let noticeColor = colors.white // Empty and reset-white by default.
+
+    // No color specifications, eg. logger('Hello world')
+    if (text === undefined) {
+        noticeColor = `${colors.white} [${returnTime()}] ${reset}`
+        return console.log(noticeColor, color)
+    }
+
+    if (color.localeCompare('info') === 0) {
+        noticeColor = `${colors.info} [${returnTime()}] [INFO]: ${reset}`
+    }
+
+    if (color.localeCompare('silly') === 0) {
+        noticeColor = `${colors.silly} [${returnTime()}] [SILLY]: ${reset}`
+    }
+
+    if (color.localeCompare('ok') === 0) {
+        noticeColor = `${colors.ok} [${returnTime()}] [OK]: ${reset}`
+    }
+
+    if (color.localeCompare('warn') === 0) {
+        // useTracer = true
+        noticeColor = `${colors.warn} [${returnTime()}] [WARN]: ${reset}`
+    }
+
+    if (color.localeCompare('debug') === 0) {
+        // useTracer = true
+        noticeColor = `${colors.debug} [${returnTime()}] [DEBUG]: ${reset}`
+    }
+
+    if (useTracer) {
+        return console.trace(noticeColor, text)
+    }
+
+    return console.log(noticeColor, text)
+}
+
